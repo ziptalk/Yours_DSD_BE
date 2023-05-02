0ce62a0859259dcd98e528e521f0bea6039a3c59
kite707 <12201680@inha.edu>
2023-05-02 17:23:36 +0900

    feat: nftController success적용

--- src/controller/nftController.ts
+++ src/controller/nftController.ts
@@ -1,9 +1,12 @@
 import { Request, Response, NextFunction } from "express";
+import responseMessage from "../module/constants/responseMessage";
+import statusCode from "../module/constants/statusCode";
+import { success } from "../module/util";
 import { nftService } from "../service";
 const mintNft = async (req: Request, res: Response, next: NextFunction) => {
   const { userId, nftGrade } = req.body;
-  await nftService.saveMintInfo(+userId, nftGrade);
-  res.send("success");
+  const data = await nftService.saveMintInfo(+userId, nftGrade);
+  success(res, statusCode.OK, responseMessage.SAVE_USER_NFT_INFO_SUCCESS, data);
 };
 
 export { mintNft };
