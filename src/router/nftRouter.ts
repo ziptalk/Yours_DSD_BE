46823a40b37dfafd368da3adfa903e7997aa8b4e
kite707 <12201680@inha.edu>
2023-05-02 18:03:31 +0900

    feat: errorValidator 적용

--- src/router/nftRouter.ts
+++ src/router/nftRouter.ts
@@ -1,6 +1,13 @@
 import { Router } from "express";
+import { body } from "express-validator";
 import { nftController } from "../controller";
+import errorValidator from "../module/error/errorValidator";
 const router: Router = Router();
 
-router.post("/", nftController.mintNft);
+router.post(
+  "/",
+  [body("userId").notEmpty(), body("nftGrade").notEmpty()],
+  errorValidator,
+  nftController.mintNft
+);
 export default router;
