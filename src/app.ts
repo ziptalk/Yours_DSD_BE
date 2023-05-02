6145af034206ccbd50b4b4dedc1ddbf42c316b2d
kite707 <12201680@inha.edu>
2023-05-02 17:07:30 +0900

    feat: express.json설정

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response, NextFunction } from "express";
 import config from "./config";
+import router from "./routes";
 
 const app = express();
+app.use(express.json());
+app.use("/api", router);
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.send("SERVER CONNECTED");
 });
