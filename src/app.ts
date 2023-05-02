944572fb1399a0176cf612e5b06e6f82f92140ff
kite707 <12201680@inha.edu>
2023-05-02 17:54:43 +0900

    feat: app.ts errorHandler 적용

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response, NextFunction } from "express";
 import config from "./config";
 import router from "./router";
+import errorHandler from "./module/error/errorHandler";
 
 const app = express();
 app.use(express.json());
 app.use("/api", router);
+app.use(errorHandler);
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.send("SERVER CONNECTED");
 });
