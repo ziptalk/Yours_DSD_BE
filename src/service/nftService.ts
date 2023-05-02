e051ab365c5f36bc76705cc75869e801ede2d5d9
kite707 <12201680@inha.edu>
2023-05-02 17:19:31 +0900

    feat: saveMintInfo 결과 반환

--- src/service/nftService.ts
+++ src/service/nftService.ts
@@ -3,12 +3,13 @@ const prisma = new PrismaClient();
 
 const saveMintInfo = async (userId: number, nftGrade: string) => {
   try {
-    await prisma.user_has_nft.create({
+    const data = await prisma.user_has_nft.create({
       data: {
         user_id: userId,
         grade: nftGrade,
       },
     });
+    return data;
   } catch (error) {
     throw error;
   }
