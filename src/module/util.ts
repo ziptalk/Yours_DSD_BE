937e61bc195674027d1a9b6434a29f7ec3373197
kite707 <12201680@inha.edu>
2023-05-02 17:19:08 +0900

    feat: util.ts에 success, fail 추가

---
+++ src/module/util.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from "express";
+
+const success = (
+  res: Response,
+  status: number,
+  message: string,
+  data?: any
+) => {
+  const result = {
+    status,
+    success: true,
+    message,
+    data,
+  };
+  res.status(status).send(result);
+};
+
+const fail = async (res: Response, status: number, message: string) => {
+  const result = {
+    status,
+    success: false,
+    message,
+  };
+  res.status(status).send(result);
+};
+export { success, fail };
