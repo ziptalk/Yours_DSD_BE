dbec5dbf316bee060abf9fd35f30eab980fe7fd1
kite707 <12201680@inha.edu>
2023-05-11 18:38:15 +0900

    feat: 통합 nft responseMessage추가

--- src/module/constants/responseMessage.ts
+++ src/module/constants/responseMessage.ts
@@ -3,4 +3,7 @@ export default {
   BAD_REQUEST: "잘못된 요청입니다.",
 
   SAVE_USER_NFT_INFO_SUCCESS: "nft 유저 정보가 성공적으로 저장되었습니다.",
+  INTEGRATE_NFT_SUCCESS: "nft 통합에 성공하였습니다.",
+
+  INSUFFICIENT_NFT: "충분한 nft를 보유하고 있지 않습니다",
 };
