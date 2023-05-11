260ccaba17b28391d8e7bb24d55ecdb48822b190
kite707 <12201680@inha.edu>
2023-05-11 18:10:45 +0900

    test: saveMintInfo 테스트 작성

---
+++ test/nftController.test.ts
@@ -0,0 +1,33 @@
+import { mintNft } from "../src/controller/nftController";
+import { statusCode, responseMessage } from "../src/module";
+import { nftService } from "../src/service";
+
+describe("mintNft : api/nft/own", () => {
+  const expectedData = {
+    userId: 1,
+    nftGrade: "tt",
+  };
+  const expectedResponse = {
+    status: statusCode.OK,
+    success: true,
+    message: responseMessage.SAVE_USER_NFT_INFO_SUCCESS,
+    data: expectedData,
+  };
+  const req: any = {
+    body: {
+      userId: 1,
+      nftGrade: "tt",
+    },
+  };
+  const res: any = {
+    status: jest.fn(() => res),
+    send: jest.fn(),
+  };
+  const next = jest.fn();
+  test("mintNft 정상 작동 테스트", async () => {
+    nftService.saveMintInfo = jest.fn().mockResolvedValue(expectedData);
+    await mintNft(req, res, next);
+    expect(res.status).toBeCalledWith(statusCode.OK);
+    expect(res.send).toBeCalledWith(expectedResponse);
+  });
+});
