d23adadc2b7c03253e7bf687f36677bd0910fb51
kite707 <12201680@inha.edu>
2023-05-15 17:44:44 +0900

    fix: errorValidator 에러 수정

--- src/module/error/errorValidator.ts
+++ src/module/error/errorValidator.ts
@@ -6,9 +6,7 @@ import { fail } from "../util";
 const errorValidator = (req: Request, res: Response, next: NextFunction) => {
   const errors: Result<ValidationError> = validationResult(req);
   if (!errors.isEmpty()) {
-    return res
-      .status(statusCode.BAD_REQUEST)
-      .send(fail(res, statusCode.BAD_REQUEST, responseMessage.ERROR_VALIDATOR));
+    return (fail(res, statusCode.BAD_REQUEST, responseMessage.ERROR_VALIDATOR));
   }
   next();
 };
