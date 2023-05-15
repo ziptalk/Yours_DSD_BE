6a5c273f6f009d8bbd84cc9ecc89c918465d5637
kite707 <12201680@inha.edu>
2023-05-15 17:26:55 +0900

    refactor: nftRouter 리팩토링

--- src/router/nftRouter.ts
+++ src/router/nftRouter.ts
@@ -2,15 +2,15 @@ import { Router } from "express";
 import { body } from "express-validator";
 import { nftController } from "../controller";
 import errorValidator from "../module/error/errorValidator";
-import { error } from "console";
 const router: Router = Router();
-console.log("라우터");
+
 router.post(
   "/own",
   [body("userId").notEmpty(), body("nftName").notEmpty()],
   errorValidator,
   nftController.mintNft
 );
+
 router.post(
   "/integrate",
   [
