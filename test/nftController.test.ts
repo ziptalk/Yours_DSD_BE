b46e330e94dc5286176f963f24c63ab3b5c7cb6e
kite707 <12201680@inha.edu>
2023-05-15 17:23:27 +0900

    test: 통합 nft 정상작동 테스트

--- test/nftController.test.ts
+++ test/nftController.test.ts
@@ -1,4 +1,4 @@
-import { mintNft } from "../src/controller/nftController";
+import { integrateNft, mintNft } from "../src/controller/nftController";
 import { statusCode, responseMessage } from "../src/module";
 import { nftService } from "../src/service";
 
@@ -27,6 +27,44 @@ describe("mintNft : api/nft/own", () => {
   test("mintNft 정상 작동 테스트", async () => {
     nftService.saveMintInfo = jest.fn().mockResolvedValue(expectedData);
     await mintNft(req, res, next);
+    expect(res.status).toBeCalledWith(statusCode.OK);
+    expect(res.send).toBeCalledWith(expectedResponse);
+  });
+});
+
+describe("integrateNft : api/nft/integrate", () => {
+  const expectedData = {
+    data: {
+      userId: 1,
+      name: "SS",
+    },
+  };
+  const expectedResponse = {
+    status: statusCode.OK,
+    success: true,
+    message: responseMessage.SUCCESS,
+    data: expectedData,
+  };
+
+  const req: any = {
+    body: {
+      userId: 1,
+      oldNfts: ["B", "B"],
+      newNft: "SS",
+    },
+  };
+
+  const res: any = {
+    status: jest.fn(() => res),
+    send: jest.fn(),
+  };
+
+  const next = jest.fn();
+
+  test("integrateNft 정상 작동 테스트", async () => {
+    nftService.deleteManyMintInfo = jest.fn();
+    nftService.saveMintInfo = jest.fn().mockReturnValue(expectedData);
+    await integrateNft(req, res, next);
     expect(res.status).toBeCalledWith(statusCode.OK);
     expect(res.send).toBeCalledWith(expectedResponse);
   });
