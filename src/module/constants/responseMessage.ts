d2e590abcbe7cfb0ccb480b561aa5a54abd40670
kite707 <12201680@inha.edu>
2023-05-15 16:15:04 +0900

    feat: 성공, errorvalidator responseMessage 추가

--- src/module/constants/responseMessage.ts
+++ src/module/constants/responseMessage.ts
@@ -1,7 +1,11 @@
 export default {
   INTERNAL_SERVER_ERROR: "서버 에러",
   BAD_REQUEST: "잘못된 요청입니다.",
+  ERROR_VALIDATOR: "errorValidator 오류 발생",
 
+  SUCCESS: "요청을 성공적으로 수행하였습니다.",
+
+  SAVE_USER_NFT_FAIL: "nft 유저 정보 저장 중 에러가 발생하였습니다.",
   SAVE_USER_NFT_INFO_SUCCESS: "nft 유저 정보가 성공적으로 저장되었습니다.",
   INTEGRATE_NFT_SUCCESS: "nft 통합에 성공하였습니다.",
 
