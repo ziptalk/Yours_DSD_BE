fdded61132b7a7e377b7417b2dcb4e0231dcbcea
kite707 <12201680@inha.edu>
2023-05-15 16:12:34 +0900

    fix: saveMintInfo prisma 오류 시 600에러 발생

--- src/service/nftService.ts
+++ src/service/nftService.ts
@@ -1,23 +1,27 @@
 import { PrismaClient } from "@prisma/client";
 import statusCode from "../module/constants/statusCode";
 import errorGenerator from "../module/error/errorGenerator";
+import { responseMessage } from "../module";
 const prisma = new PrismaClient();
 
-const saveMintInfo = async (userId: number, nftGrade: string) => {
+const saveMintInfo = async (userId: number, nftName: string) => {
   try {
     const user = await prisma.user_has_nft.create({
       data: {
         user_id: userId,
-        grade: nftGrade,
+        name: nftName,
       },
     });
     const data = {
       userId: user.user_id,
-      nftGrade: user.grade,
+      name: user.name,
     };
     return data;
   } catch (error) {
-    throw errorGenerator({ statusCode: statusCode.INTERNAL_SERVER_ERROR });
+    throw errorGenerator({
+      msg: responseMessage.SAVE_USER_NFT_FAIL,
+      statusCode: statusCode.DB_ERROR,
+    });
   }
 };
 
