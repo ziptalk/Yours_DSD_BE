ae8802ac4e8f7315a69cf3ee21d8244595b34303
kite707 <12201680@inha.edu>
2023-05-15 16:13:12 +0900

    fix: nft정보저장 라우터 db컬럼이름 변경 적용

--- src/router/nftRouter.ts
+++ src/router/nftRouter.ts
@@ -2,12 +2,15 @@ import { Router } from "express";
 import { body } from "express-validator";
 import { nftController } from "../controller";
 import errorValidator from "../module/error/errorValidator";
+import { error } from "console";
 const router: Router = Router();
-
+console.log("라우터");
 router.post(
   "/own",
-  [body("userId").notEmpty(), body("nftGrade").notEmpty()],
+  [body("userId").notEmpty(), body("nftName").notEmpty()],
   errorValidator,
   nftController.mintNft
 );
+
+
 export default router;
