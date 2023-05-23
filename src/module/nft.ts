315b5613a092e2ce78f620ac5bc4fe8f43209c3d
kite707 <12201680@inha.edu>
2023-05-23 10:26:10 +0900

    feat: mintNft모듈 구현

--- src/module/nft.ts
+++ src/module/nft.ts
@@ -1,11 +1,8 @@
+import { ethers } from "ethers";
 import { uploadMetaIpfs } from "../contract/commonContract";
-import { deployMumbaiNFT } from "../contract/mumbaiContract";
-import {
-  finishLoading,
-  getNftInfo,
-  saveNftAddress,
-  startLoading,
-} from "../service/nftService";
+import { deployMumbaiNFT, mintMumbaiNFT, polygonProvider } from "../contract/mumbaiContract";
+import { finishLoading, getNftInfo, saveMintId, saveNftAddress, startLoading } from "../service/nftService";
+import dsdBenefitData from "../contract/DSDBenefitNFT.json";
 import responseMessage from "./constants/responseMessage";
 import statusCode from "./constants/statusCode";
 import errorGenerator from "./error/errorGenerator";
@@ -13,12 +10,7 @@ import errorGenerator from "./error/errorGenerator";
 const deployNFT = async (nftName: string) => {
   try {
     const nftInfo = await getNftInfo(nftName);
-    const metaUri = await uploadMetaIpfs(
-      nftInfo.name,
-      nftInfo.description!,
-      nftInfo.image!,
-      nftInfo.video!
-    );
+    const metaUri = await uploadMetaIpfs(nftInfo.name, nftInfo.description!, nftInfo.image!, nftInfo.video!);
 
     /**nft 발행 시작 */
     await startLoading(nftName);
@@ -32,3 +24,19 @@ const deployNFT = async (nftName: string) => {
     });
   }
 };
+
+const mintNft = async (nftName: string, receiverAddress: string, userId: number) => {
+  try {
+    const nftInfo = await getNftInfo(nftName);
+
+    const nftContract = new ethers.Contract(nftInfo.nftAddress as string, dsdBenefitData.abi, polygonProvider);
+    const mintData = await mintMumbaiNFT(nftContract, receiverAddress);
+    await saveMintId(nftName, userId, mintData.mintId);
+  } catch (error) {
+    throw errorGenerator({
+      msg: responseMessage.MINT_NFT_FAIL,
+      statusCode: statusCode.BAD_REQUEST,
+    });
+  }
+};
+export { deployNFT, mintNft };
