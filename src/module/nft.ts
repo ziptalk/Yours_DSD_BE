bd85d9932f8b9afa7208e1317dac84680054dc73
kite707 <12201680@inha.edu>
2023-05-26 17:20:31 +0900

    feat: 민팅된 nft, 민팅되지 않은 nft 조회 함수 적용

--- src/module/nft.ts
+++ src/module/nft.ts
@@ -44,13 +44,14 @@ const deployNFT = async (nftName: string) => {
 /**nft모듈: nft민팅 */
 const mintNft = async (nftName: string, receiverAddress: string, userId: number) => {
   /**web2상에서 가지고있는지 체크하고 가지고있으면 민팅 시작.*/
-  const userNft = await nftService.getUserNftInfo(nftName, userId);
+  const userNft = await nftService.getUnmintedUserNftInfo(nftName, userId);
   if (!userNft) {
     throw errorGenerator({
       msg: responseMessage.INSUFFICIENT_NFT,
       statusCode: statusCode.BAD_REQUEST,
     });
   }
+  /**로딩여부 체크 */
   const nftInfo = await nftService.getNftInfo(nftName);
   const nftContract = new ethers.Contract(
     nftInfo.nftAddress as string,
@@ -71,16 +72,22 @@ const mintNft = async (nftName: string, receiverAddress: string, userId: number)
 const burnNft = async (nftName: string, userId: number) => {
   try {
     const nftInfo = await nftService.getNftInfo(nftName);
-    const ownedNftInfo = await nftService.getUserNftInfo(nftName, userId);
-
+    /**민팅된 nft 조회 */
+    const userNft = await nftService.getMintedUserNftInfo(nftName, userId);
+    if (!userNft) {
+      throw errorGenerator({
+        msg: responseMessage.INSUFFICIENT_NFT,
+        statusCode: statusCode.BAD_REQUEST,
+      });
+    }
     const nftContract = new ethers.Contract(
       nftInfo.nftAddress as string,
       dsdBenefitData.abi,
       polygonProvider,
     );
 
-    const burnInfo = await burnNFT(nftContract, ownedNftInfo?.mint_id!);
-    await nftService.addBurnInfo(ownedNftInfo?.id!);
+    const burnInfo = await burnNFT(nftContract, userNft?.mint_id!);
+    await nftService.addBurnInfo(userNft?.id!);
     return burnInfo.transactionHash;
   } catch (error) {
     console.log(error);
