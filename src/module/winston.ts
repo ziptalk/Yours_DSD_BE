81b7dd85fbdaa7cf7cd10219c7b81637fd490aaf
kite707 <12201680@inha.edu>
2023-05-29 19:13:10 +0900

    feat: winston 모듈 추가

---
+++ src/module/winston.ts
@@ -0,0 +1,77 @@
+import winston from "winston";
+import "winston-daily-rotate-file";
+import config from "../config";
+import moment from "moment-timezone";
+const logDir = "./logs";
+
+const levels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  debug: 4,
+};
+
+const level = () => {
+  const env = config.env || "development";
+  const isDevelopment = env === "development";
+  return isDevelopment ? "debug" : "warn";
+};
+
+const colors = {
+  error: "red",
+  warn: "yellow",
+  info: "green",
+  http: "magenta",
+  debug: "blue",
+};
+
+winston.addColors(colors);
+
+const printFormat = winston.format.printf((info) => {
+  let logString = `${info.timestamp} ${info.level}  -> ${info.message}`;
+  if (info.level == "error") {
+    logString += `\n ${info.stack}`;
+  }
+  return logString;
+});
+
+const format = winston.format.combine(
+  winston.format.timestamp({ format: " YYYY-MM-DD HH:MM:SS ||" }),
+  printFormat,
+);
+
+const logger = winston.createLogger({
+  format,
+  level: level(),
+  transports: [
+    new winston.transports.DailyRotateFile({
+      level: "info", // info 레벨 이상( error, warn, info) 에 대한 모든 로그 저장
+      datePattern: "YYYY-MM-DD",
+      dirname: logDir,
+      filename: `%DATE%.log`,
+      zippedArchive: true,
+      handleExceptions: true,
+      maxFiles: 30, // 30일치 로그 파일
+    }),
+    new winston.transports.DailyRotateFile({
+      level: "error", // error가 최상위 레벨이므로 에러에 대한 로그만 따로 저장
+      datePattern: "YYYY-MM-DD",
+      dirname: logDir + "/error",
+      filename: `%DATE%.error.log`,
+      zippedArchive: true,
+      maxFiles: 30, // 30일치 로그 파일
+    }),
+    new winston.transports.Console({
+      handleExceptions: true,
+    }),
+  ],
+});
+moment.tz.setDefault("Asia/Seoul"); // 로그 시간대 한국 기준으로 변경
+const timeStamp = () => moment().format("YYYY-MM-DD HH:mm:ss");
+const stream = {
+  write: (message: string) => {
+    logger.info(`${timeStamp()} ${message}`);
+  },
+};
+export { logger, stream };
