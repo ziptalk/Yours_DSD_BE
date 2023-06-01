d8a324ecf7465a473d95fe13ddcaebd7d08aa7f0
kite707 <12201680@inha.edu>
2023-06-01 18:42:50 +0900

    feat: morgan적용

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import express, { Request, Response, NextFunction } from "express";
 import config from "./config";
 import router from "./router";
 import errorHandler from "./module/error/errorHandler";
-
+import morgan from "morgan";
+import { stream } from "./module/winston";
 const app = express();
 app.use(express.json());
+app.use(morgan("combined", { stream }));
 app.use("/api", router);
 app.use(errorHandler);
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
